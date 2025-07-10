1b04219c79fba3e8b224d1a9af439cb8d8e5dd44
newmaster <2338946313@qq.com>
2025-07-10 17:26:48 +0800

    feat(controller): 添加聊天流式接口并优化编码
    - 新增 /stream/chat 接口，支持流式聊天响应

--- src/main/java/com/newmaster/controller/ChatController.java
+++ src/main/java/com/newmaster/controller/ChatController.java
@@ -1,11 +1,13 @@
 package com.newmaster.controller;
 
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
 
 @RestController
 @RequiredArgsConstructor
@@ -17,6 +19,16 @@ public class ChatController {
 	@GetMapping("/simple/chat")
 	public String chat(@RequestParam(value = "query", defaultValue = "你好，很高兴认识你，能简单介绍一下自己吗？") String query) {
 		return chatClient.prompt(query).call().content();
+	}
+	
+	@GetMapping("/stream/chat")
+	public Flux<String> streamChat(
+			@RequestParam(value = "query", defaultValue = "你好，很高兴认识你，能简单介绍一下自己吗？") String query,
+			HttpServletResponse response) {
+		
+		response.setCharacterEncoding("UTF-8");
+		
+		return chatClient.prompt(query).stream().content();
 	}
 }
 
